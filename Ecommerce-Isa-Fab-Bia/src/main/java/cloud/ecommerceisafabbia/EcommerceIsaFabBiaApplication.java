117af0a874576b873001b0ae25652699a60ff4a7
beatrizsantosvieira <beatrizvieiracorp@gmail.com>
2025-04-05 23:30:45 -0300

    Corrigido erro de injeção de dependência no PedidoController.

--- Ecommerce-Isa-Fab-Bia/src/main/java/cloud/ecommerceisafabbia/EcommerceIsaFabBiaApplication.java
+++ Ecommerce-Isa-Fab-Bia/src/main/java/cloud/ecommerceisafabbia/EcommerceIsaFabBiaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("cloud.ecommerceisafabbia.repositorioJPA") // Escaneia os repositórios no pacote correto
+@EnableJpaRepositories("cloud.ecommerceisafabbia.repositorioJPA") // Certifique-se de que o pacote está correto
 public class EcommerceIsaFabBiaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EcommerceIsaFabBiaApplication.class, args);
